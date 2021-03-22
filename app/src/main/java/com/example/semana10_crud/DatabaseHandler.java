e897329129ba67c5bc0cdfc50e608728522c61eb
Gisela <gys.espinoza@gmail.com>
2021-03-22 09:04:38 -0600

    CRUD Con SQLite Se agrego insertData y getData, se crea interfaz

---
+++ app/src/main/java/com/example/semana10_crud/DatabaseHandler.java
@@ -0,0 +1,50 @@
+package com.example.semana10_crud;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    //constructor
+    public DatabaseHandler(Context context) {
+        super(context, "registro.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE persona(idPersona TEXT PRIMARY KEY, nombre TEXT, apellido TEXT)"); //crear la tabla de la base de datos
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS persona");
+    }
+
+    //metodos CRUD
+    public Boolean insertData(String idPersona, String nombre, String apellido){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("idPersona", idPersona);
+        contentValues.put("nombre", nombre);
+        contentValues.put("apellido", apellido);
+        long result= db.insert("persona", null, contentValues);
+        if(result==-1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    //obtener datos
+    public Cursor getData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor=db.rawQuery("SELECT * FROM persona", null);
+        return  cursor;
+    }
+
+}
